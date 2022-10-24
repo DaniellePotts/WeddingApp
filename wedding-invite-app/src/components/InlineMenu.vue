8a37fd125980a27562c7213159701f17f390a92f
Potts <danielle.potts@accenture.com>
2022-10-24 15:57:05 +0100

    added venue page setup

---
+++ wedding-invite-app/src/components/InlineMenu.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+
+</script>
+
+<template>
+<div class="inline-menu">
+    <ul>
+  
+  <li><RouterLink to="/home">Home</RouterLink></li>
+  <li><RouterLink to="/venue">Venue</RouterLink></li>
+
+  <li><RouterLink to="/contact">Contact</RouterLink></li>
+  <li> <RouterLink to="/invites">Invites</RouterLink></li>
+ 
+
+</ul>
+</div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
--- wedding-invite-app/src/views/VenueView.vue
+++ wedding-invite-app/src/views/VenueView.vue
@@ -1,22 +1,115 @@
+<script>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
 <template>
   <div class="venue">
-    <h1>This is the information about the venue</h1>
-    <p>Roker Hotel, BW Premier Collection</p>
-    <p>Roker Terrace</p>
-    <p>Roker</p>
-    <p>Sunderland</p>
-    <p>SR6 9ND</p>
-    <p>Phone: 0191 567 1786</p>
-    <p>Rooms can be booked at a discounted price if you say it's for our wedding</p>
+    <ul>
+
+      <li>
+        <RouterLink to="/home">Home</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/venue">Venue</RouterLink>
+      </li>
+
+      <li>
+        <RouterLink to="/contact">Contact</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/invites">Invites</RouterLink>
+      </li>
+
+
+    </ul>
+
+    <div class="row">
+
+      <div class="column" style="background-color:#f8f8f6;">
+        <iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen
+          referrerpolicy="no-referrer-when-downgrade"
+          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyD9YauwtDfU_cPN1bWMnf0Jl0y1x9c1MSs&q=Space+Needle,Roker+Hotel+Sunderland">
+        </iframe>
+      </div>
+      <div class="column" style="background-color:#f8f8f6;">
+        <div class=" venue-info">
+          <div class="address">
+            <h1>Roker Hotel, BW Premier Collection</h1>
+            <p>Roker Terrace</p>
+            <p>Roker</p>
+            <p>Sunderland</p>
+            <p>SR6 9ND</p>
+            <p>Phone: 0191 567 1786</p>
+            <p>Website: <a href="https://rokerhotel.co.uk/" target="_blank">https://rokerhotel.co.uk/</a></p>
+            <p>Rooms can be booked at a discounted price if you say it's for our wedding</p>
+
+          </div>
+          <div class="parking-traveling">
+            <h1>Parking & Traveling</h1>
+            <p>There is parking located at the front and back of the hotel.</p>
+            <p>To travel via public transport, St Peters Metro is the closest station. Alternatively, buses to Roker can
+              be found <a target="_blank" href="https://bustimes.org/localities/roker">here</a></p>
+
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .venue {
-    /* min-height: 100vh;
-    display: flex;
-    align-items: center; */
-  }
+  .venue {}
+}
+
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #04AA6D;
+}
+
+.venue-info {
+  margin-top: 10%;
+  text-align: right;
+  color: #4c5747
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.row {
+  display: flex;
+}
+
+/* Create two equal columns that sits next to each other */
+.column {
+  flex: 50%;
+  padding: 10px;
+  height: 100%;
+  /* Should be removed. Only for demonstration */
 }
 </style>
