a8a5e34a3ed25fba01d066603a05db58c2ecbcb5
Pett <warren.pett@accenture.com>
2022-10-26 08:53:04 +0100

    Added an order

--- wedding-invite-app/src/components/RSVP/RSVP.vue
+++ wedding-invite-app/src/components/RSVP/RSVP.vue
@@ -1,12 +1,12 @@
 <!-- eslint-disable prettier/prettier -->
 <template>
     <a-row type="flex" justify="space-around" align="middle">
-        <a-col :span="18">
+        <a-col :span="18" :xs="{ order: 1 }" :sm="{ order: 1 }" :md="{ order: 1 }" :lg="{ order: 1 }">
             <a-image
                 src="https://media-cldnry.s-nbcnews.com/image/upload/newscms/2019_45/2619891/181026_penguin_couple.jpg"
             />
         </a-col>
-        <a-col :span="6" align="middle">
+        <a-col :span="6" :xs="{ order: 2 }" :sm="{ order: 2 }" :md="{ order: 2 }" :lg="{ order: 2 }" align="middle">
             <p>Roker Hotel, Sunderland</p>
             <p>Friday, September 8th 2023</p>
             <a-button type="primary" ghost>Primary</a-button>
